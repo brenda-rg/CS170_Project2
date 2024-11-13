f30fc59d3ffcdfe831973bc44b7ddd82a4b2603b
Cserr170 <crisserr03@gmail.com>
2024-11-13 14:31:00 -0800

    node and main start

---
+++ Node.cpp
@@ -0,0 +1,13 @@
+#ifndef NODE_CPP_
+#define NODE_CPP_
+
+#include "node.h"
+#include <math.h>
+using namespace std;
+
+
+Node::Node() {
+    accuracy = 0;
+};
+
+#endif
---
+++ Node.h
@@ -0,0 +1,16 @@
+#ifndef NODE_H_
+#define NODE_H_
+
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+struct Node {
+    Node();
+    double accuracy;
+    Node* head = nullptr;
+
+};
+
+#endif 
